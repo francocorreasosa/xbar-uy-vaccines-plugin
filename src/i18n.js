2522b75c5454c4d7f5f5f2d33415290011c1f7d3
Franco Correa <4152942+francocorreasosa@users.noreply.github.com>
2021-04-01 17:02:10 -0300

    Add chinese as a i18n option

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,24 @@ const config = require("../config");
 const STRINGS = {
   en: {
     today: "Today:",
-    progress_single_dose: "Progress (single dose):",
+    progress_both_doses: "Progress (both doses):",
     vaccines_applied: "Vaccines applied:",
     latest_update: "Latest MSP update:",
     date_locale: "en-US",
   },
   es: {
     today: "Hoy:",
-    progress_single_dose: "Progreso (una dosis):",
+    progress_both_doses: "Progreso (ambas dosis):",
     vaccines_applied: "Vacunas aplicadas:",
     latest_update: "Actualización del MSP:",
     date_locale: "es-UY",
+  },
+  cn: {
+    today: "今天：",
+    progress_both_doses: "进展（两剂）：",
+    vaccines_applied: "应用的疫苗：",
+    latest_update: "MSP更新：",
+    date_locale: "cn-CN",
   },
 };
 
